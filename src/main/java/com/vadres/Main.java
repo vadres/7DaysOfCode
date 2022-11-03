6830ec95c89b16ed317f8b7798e3ae626a755c3a
Victor Costa <antonio.passos@grupomateus.com.br>
2022-11-03 11:05:32 -0300

    feat: transform json using only java vanilla

--- src/main/java/com/vadres/Main.java
+++ src/main/java/com/vadres/Main.java
@@ -8,11 +8,17 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 	private static Logger logger = Logger.getLogger(Main.class.getName());
 	private static String url = "https://imdb-api.com/en/API/Top250Movies/%s";
+	private static final String PATTERN_TITLES = "\"title\":\"([^\"]+)\"";
+	private static final String PATTERN_IMAGES = "\"image\":\"([^\"]+)\"";
 
 	public static void main(String ...args) throws URISyntaxException, IOException, InterruptedException {
 		Dotenv dotenv = Dotenv.configure().load();
@@ -25,7 +31,24 @@ public class Main {
 				.GET()
 				.build();
 
-		String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
-		logger.info(response);
+		String json = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+
+		List<String> titles = parseJson(json, PATTERN_TITLES);
+		List<String> urlImages = parseJson(json, PATTERN_IMAGES);
+
+		logger.info(titles::toString);
+		logger.info(urlImages::toString);
+	}
+
+	private static List<String> parseJson(String json, String patternString) {
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(json);
+
+		List<String> titles = new ArrayList<>();
+		while(matcher.find()) {
+			titles.add(matcher.group(1));
+		}
+
+		return titles;
 	}
 }
