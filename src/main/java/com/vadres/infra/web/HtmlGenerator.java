5f6c31b3950e296d3a41ed5404a1364fffeb3bbd
Victor Passos <vpassos10@hotmail.com>
2022-11-06 12:19:48 -0300

    refact: edit html style

--- src/main/java/com/vadres/infra/web/HtmlGenerator.java
+++ src/main/java/com/vadres/infra/web/HtmlGenerator.java
@@ -16,29 +16,38 @@ public class HtmlGenerator {
 		writer.println("""
             <html>
                 <head>
-                    <meta charset="utf-8">
+                     <meta charset="utf-8">
                      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
                 </head>
                 <body>
+                    <div class="container">
+                    <div class="row">
         """);
 
 		for (Movie movie : movies) {
 			String div = """
-                  <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-                       <h4 class="card-header">%s</h4>
+                  <div class="col-lg-4 col-md-3 col-sm-2"  style="padding: 5px 15px">
+                  <div class="card" style="min-height: 100%%;">
+                       <img class="card-img-top" src="%s" alt="%s">
                        <div class="card-body">
-                           <img class="card-img" src="%s" alt="%s">
-                           <p class="card-text mt-2">Nota: %s - Ano: %s</p>
+                           <h4 class="card-title">%s</h4>
+                           <h5 class="card-text mt-2"  style="display:flex;justify-content:space-between">
+                               <div>Nota: <span class="badge bg-success">%s</span></div> 
+                               <div>Ano: <span class="badge bg-primary">%s</div>
+                           </h5>
                        </div>
+                  </div>
                   </div>
              """;
 
-			writer.println(String.format(div, movie.title(), movie.url(), movie.title(), movie.rating(), movie.year()));
+			writer.println(String.format(div, movie.url(), movie.title(), movie.title(), movie.rating(), movie.year()));
 		}
 
 
 		writer.println("""
+                </div>
+                </div>
              </body>
              </html>
         """);
